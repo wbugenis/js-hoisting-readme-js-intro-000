fb12cafd7f9fa8bf8ae55ed0b4c4e2656d895cfe
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-09-28 04:09:03 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,16 +3,15 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
 }
-
 
 function crazy() {
   // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
+  function thisIsCrazy() {
     console.log("hey!!!")
   }
 }
@@ -24,7 +23,7 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
 }
